62e377e810d3ff3c3297fe5bf13a4d98390b70f7
FELIPE FELIX GAVIOLLE <00001106631547sp@aluno.educacao.sp.gov.br>
2024-06-04 09:25:28 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,49 +1,78 @@
 function dividirNumeros(){
-    var numero1 = parseInt(document.getElementById("num1").value);
-    var numero2 = parseInt(document.getElementById("num2").value);
-    var resultadoDividir = numero1 / numero2
-    var teste = document.getElementById("resultadoDividir")
-    teste.innerHTML=resultadoDividir
-    if((numero2==0)){
-        alert("seu numero nao pode ser 0, digite outro numero" )
+  var numero1 = parseInt(document.getElementById("num1").value);
+  var numero2 = parseInt(document.getElementById("num2").value);
+  var resultadoDividir = numero1 / numero2
+  var teste = document.getElementById("resultadoDividir")
+  teste.innerHTML=resultadoDividir
+  if((numero2==0)){
+      alert("seu numero n pode ser 0, digite outro")
+  }
 }
-}
-function mediaNotas(){
-    var numero3 =parseInt(document.getElementById("num3").value);
-    var numero4 =parseInt(document.getElementById("num4").value);
-    var numero5 =parseInt(document.getElementById("num5").value);
-    var numero6 =parseInt(document.getElementById("num6").value);
-    var resultado = numero3+numero4+numero5+numero6 ;
-    var media = resultado/4
-    if (media>5){
-        alert("Você está aprovado,sua nota é " +media)
-      }
-      else{
-        alert("você está reprovado,pois sua nota é "+media)
-      }
-    
-    }
 
-    function somarNumeros(){
-    
-        var numero1 = parseInt(document.getElementById("numero1").value);
-        var numero2 = parseInt(document.getElementById("numero2").value);
-        var numero3 = parseInt(document.getElementById("numero3").value);
-        var resultado = numero1 + numero2
-      if(numero3<resultado){
-        alert(resultado+ "é maior que" +numero3)
-      }
-      else{
-        alert(resultado+ "é menor que " +numero3)
-      }
-    }
-    function retangulo(){
-        var numero11 = parseInt(document.getElementById("nume1").value);
-        var numero12 = parseInt(document.getElementById("nume2").value);
-        var resultado = numero11 * numero12
-        var teste = document.getElementById("resultadoretangulo")
-        teste.innerHTML=resultado
-    }
-    
+function notaNumeros(){
+  var nota1 = document.getElementById("nota1").value
+  var nota1Alterado = parseInt(nota1)
+  var nota2 = document.getElementById("nota2").value
+  var nota2Alterado = parseInt(nota2)
+  var nota3 = document.getElementById("nota3").value
+  var nota3Alterado = parseInt(nota3)
+  var nota4 = document.getElementById("nota4").value
+  var nota4Alterado = parseInt(nota4)
+
+  var resultado = (nota1Alterado+nota2Alterado+nota3Alterado+nota4Alterado)/4
+
+  if(resultado>5){
+      alert("o aluno foi aprovado com: "+resultado)
+  }
+
+  else{
+      alert("O aluno foi reprovado com: "+resultado)
+  }
+
+}
+
+function somarNumeros(){
+  var numero1 = document.getElementById("num10").value
+  var numero1Alterado = parseInt(numero1)
+  var numero2 = document.getElementById("num11").value
+  var numero2Alterado = parseInt(numero2)
+  var numero3 = document.getElementById("num12").value
+  var numero3Alterado = parseInt(numero3)
+  var resultado = numero1Alterado+numero2Alterado
+  if(numero3Alterado<resultado){
+      alert(resultado+ " é maior que " +numero3Alterado)
+  }
+  else{
+      alert(resultado+ " é menor que " +numero3Alterado)
+  }
+
+}
+
+function retangulo(){
   
+  var numero13 = parseInt(document.getElementById("num13").value);
+  var numero14 = parseInt(document.getElementById("num14").value);
+  var resultado = numero13 * numero14
+  var teste = document.getElementById("resultadoretangulo")
+  teste.innerHTML=resultado
+}
+
+function reajuste(){
+  var numero15 = parseInt(document.getElementById("num15").value);
+  var numero16 = parseInt(document.getElementById("porc").value);
+  var resultado = numero15*(numero16/100)+numero15
+  var teste = document.getElementById("salarioAtual")
+  teste.innerHTML=resultado
   
+}
+
+function pinto(){
+  var numero17 = parseInt(document.getElementById("num17").value);
+  if(numero17<10){
+      alert("o numero digitado é menor que 10")
+  }
+  else{
+      alert("o numero digitado é maior que 10")
+  }
+  
+}
